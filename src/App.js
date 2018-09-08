27f2d14c4e8c31ef9ee2baf0180d9dfdee6d6cd1
Anthony Hernandez <tony91139@gmail.com>
2018-09-07 23:31:48 -0700

    Update app with temp message re: intentions

--- src/App.js
+++ src/App.js
@@ -7,11 +7,23 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {
+            //<img src={logo} className="App-logo" alt="logo" />}
+          }
+
+          <h1 className="App-title">Consuming the Pinterest API</h1>
         </header>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Going to build an app that will be used to acquire access to the
+          Pinterest API.
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          Come back soon!
         </p>
       </div>
     );

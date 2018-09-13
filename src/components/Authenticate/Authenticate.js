42aae498d696417f686ee597bdc19c7d47932eed
Anthony Hernandez <tony91139@gmail.com>
2018-09-13 01:15:18 -0700

    I dont even know

--- src/components/Authenticate/Authenticate.js
+++ src/components/Authenticate/Authenticate.js
@@ -15,7 +15,7 @@ class UserSessionControls extends React.Component {
   };
 
   login() {
-    Pinterest.login({ scope : 'read_public' }, this.props.onAuthenticate());
+    Pinterest.login({ scope : 'read_public' }, this.props.onAuthenticate);
   };
 
   resetState() {
@@ -32,7 +32,7 @@ class UserSessionControls extends React.Component {
         <button
           className="logout-button"
           onClick={() => {
-            Pinterest.logout(this.props.onAuthenticate());
+            Pinterest.logout(this.props.onAuthenticate;
           }}
         >
           Sign out
